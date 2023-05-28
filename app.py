3f1b6075dfd435e1e891ac796322e9e77b59edcd
Jingye Chen <576194329@qq.com>
2023-05-29 03:40:55 +0800

    update

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import zipfile
 
 
 os.system('wget https://layoutlm.blob.core.windows.net/textdiffuser/textdiffuser-ckpt.zip')
-
+os.system('wget https://layoutlm.blob.core.windows.net/textdiffuser/Arial.ttf')
 # 打开 zip 文件
 with zipfile.ZipFile('textdiffuser-ckpt.zip', 'r') as zip_ref:
     zip_ref.extractall('.')
